466e898b0081858634c675b6457ce3e0294b6f0e
Jaron Aviles <jaron.aviles@gmail.com>
2025-06-07 16:36:55 -0600

    Se agrego la animacion de vista del sprite del personaje principal

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movimiento : MonoBehaviour
@@ -9,15 +6,20 @@ public class Movimiento : MonoBehaviour
     [SerializeField] private Vector2 direccion;
 
     private Rigidbody2D rb;
+    private Animator anim;
 
     public void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     public void Update()
     {
         direccion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        anim.SetFloat("MoveX", direccion.x);
+        anim.SetFloat("MoveY", direccion.y);
+        anim.SetBool("IsMoving", direccion.magnitude > 0);
     }
     
     public void FixedUpdate()
